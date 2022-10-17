dba35f19bd4a02eb60f3658a2d9e469a879306ad
jazi1001 <jazi1001@stud.hs-kl.de>
2022-10-17 11:53:20 +0200

    Uebung_01

--- Uebung01/src/aufgabe2/Student.java
+++ Uebung01/src/aufgabe2/Student.java
@@ -1,10 +1,44 @@
 package aufgabe2;
 
 public class Student {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+private String Vorname;
+   
+   public void setVorname(String value) {
+      this.Vorname = value;
+   }
+   
+   public String getVorname() {
+      return this.Vorname;
+   }
+   
+   private String Nachname;
+   
+   public void setNachname(String value) {
+      this.Nachname = value;
+   }
+   
+   public String getNachname() {
+      return this.Nachname;
+   }
+   
+   private String Studentenkürzel;
+   
+   public void setStudentenkürzel(String value) {
+      this.Studentenkürzel = value;
+   }
+   
+   public String getStudentenkürzel() {
+      return this.Studentenkürzel;
+   }
+   
+   public String getName() {
+      return this.Nachname+","+this.Vorname;
+   }
+   
+   public Student (String Vorname, String Nachname) {
+      this.Vorname = Vorname;
+      this.Nachname = Nachname;
+   }
+   
+   
 }
